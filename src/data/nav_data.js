09eda2813b175b3efc24e520597f043ba882b589
Amya Moore <bookworm7572@gmail.com>
2023-11-29 02:45:45 -0500

    Updated nav.astro
    Changed navigation to data mapping
    Added pages

---
+++ src/data/nav_data.js
@@ -0,0 +1,17 @@
+const nav_data = [
+	{
+		name: 'Home',
+		path: '',
+	},
+	{
+		// TODO: link to Work section header
+		name: 'Work',
+		path: '',
+	},
+	{
+		name: 'Profile',
+		path: '/profile/',
+	},
+]
+
+export default nav_data;
