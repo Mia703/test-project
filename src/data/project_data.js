65edac36ec7ded8e9157d76733e8658ea035fa9a
Amya Moore <bookworm7572@gmail.com>
2023-12-04 19:15:42 -0500

    Added project outline

--- src/data/project_data.js
+++ src/data/project_data.js
@@ -1,16 +1,83 @@
 const project_data = [
 	{
 		id: 0,
-		projectName: "Tutorial",
-		projectDescription: "this is a tutorial description",
-		projectGitLink: "",
+		projectName: "Testimonial Grid Challenge",
+		projectDescription:
+			"Challenged by Front-end Mentor to build a testimonials grid section using HTML, CSS, and CSS Grid.",
+		projectCreationDate: "December 8, 2022",
+		projectGitLink: "https://www.google.com/",
 		projectLiveLink: "",
 		projectImage: "",
 	},
 	{
 		id: 1,
-		projectName: "Tutorial2",
-		projectDescription: "this is a tutorial description",
+		projectName: "News Homepage Challenge",
+		projectDescription:
+			"Challenged by Front-end Mentor to build a news website homepage, using HTML, CSS, and CSS grid.",
+		projectGitLink: "",
+		projectLiveLink: "",
+		projectImage: "",
+	},
+	{
+		id: 2,
+		projectName: "CS Department Re-Design",
+		projectDescription:
+			"As a in-team class assignment, re-designed the Computer Science department page at Emory University.",
+		projectCreationDate: "",
+		projectGitLink: "",
+		projectLiveLink: "",
+		projectImage: "",
+	},
+	{
+		id: 3,
+		projectName: "UFO Sightings Visualisation",
+		projectDescription: "",
+		projectCreationDate: "",
+		projectGitLink: "",
+		projectLiveLink: "",
+		projectImage: "",
+	},
+	{
+		id: 4,
+		projectName: "VR Headset Experience",
+		projectDescription: "",
+		projectCreationDate: "",
+		projectGitLink: "",
+		projectLiveLink: "",
+		projectImage: "",
+	},
+	{
+		id: 5,
+		projectName: "Becca Fashion Chatbot",
+		projectDescription: "",
+		projectCreationDate: "",
+		projectGitLink: "",
+		projectLiveLink: "",
+		projectImage: "",
+	},
+	{
+		id: 6,
+		projectName: "Defining Authenticity",
+		projectDescription: "",
+		projectCreationDate: "",
+		projectGitLink: "",
+		projectLiveLink: "",
+		projectImage: "",
+	},
+	{
+		id: 7,
+		projectName: "Simon Game",
+		projectDescription: "A recreation of the classic memory game Simon.",
+		projectCreationDate: "",
+		projectGitLink: "",
+		projectLiveLink: "",
+		projectImage: "",
+	},
+	{
+		id: 8,
+		projectName: "Marketing for University of Denver's Art and Art History Department",
+		projectDescription: "",
+		projectCreationDate: "",
 		projectGitLink: "",
 		projectLiveLink: "",
 		projectImage: "",
@@ -18,3 +85,15 @@ const project_data = [
 ];
 
 export default project_data;
+
+/* 
+	{
+		id: 0,
+		projectName: "",
+		projectDescription: "",
+		projectCreationDate: "",
+		projectGitLink: "",
+		projectLiveLink: "",
+		projectImage: "",
+	},
+*/
