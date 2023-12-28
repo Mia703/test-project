57e5b001a32254221b256d5a83abb0ecb8c24d44
Amya Moore <bookworm7572@gmail.com>
2023-12-27 23:40:18 -0500

    Added StackBlitz
    Added the sandbox I was working on in StackBlitz

---
+++ src/data/experience.js
@@ -0,0 +1,30 @@
+const work_history_list = [
+	{
+		key: 3,
+		position: "Digital Marketing and Design Assistant",
+		company: "University of Denver, School of Art and Art History",
+		date: "September 2023 - Present",
+	},
+	{
+		key: 2,
+		position: "IT and CRM Assistant",
+		company: "Higher Education Resource Services (HERS)",
+		date: "September 2023 - Present",
+	},
+	{
+		key: 1,
+		position: "Props and Painting Assistant",
+		company: "Emory University, Theatre Emory",
+		date: "August 2022 - May 2023",
+	},
+	{
+		key: 0,
+		position: "Part-time Store Associate",
+		company: "Khols",
+		date: "June 2023 - July 2023",
+	},
+];
+
+const most_recent_positions = work_history_list.slice(0, 3);
+
+export default most_recent_positions;
